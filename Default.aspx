7107f3e847885f5769d72a89ecb26d9158de71e3
Kobusvds <kobusvdsandt@gmail.com>
2018-02-12 00:03:06 +0200

    Upload

---
+++ Default.aspx
@@ -0,0 +1,101 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Customer Contact Management</title>
+    <link href="k2.css" rel="stylesheet" />
+</head>
+<body class="bg-light-grey">
+    <form id="form1" runat="server">
+    <asp:HiddenField ID="hidCust" runat="server" />
+    <asp:HiddenField ID="hidCon" runat="server" />
+
+    <!-- HEADER -->
+    <div class="p-3 bg-white s-2">Customer Contact Manager</div>
+
+    <!-- BODY -->
+    <div class="container-fluid pt-3" align="center">
+        <asp:Label ID="lblError" runat="server" Text="" CssClass="text-red" /><br/>
+
+        <!-- CUSTOMERS -->
+        <div class="d-inline-flex flex-row flex-wrap">
+            <div class="p-3">
+
+                <!-- CUSTOMERS GRID -->
+                <b>Customers</b> 
+                <asp:Button ID="btnCustAdd" runat="server" CssClass="btn btn-sm btn-dark ml-2" Text="Add Customer" OnClick="btnCustAdd_Click" />
+                <asp:GridView ID="gvCustomers" runat="server" CssClass="tbl s-2 my-3" AutoGenerateColumns="False" DataKeyNames="ID" OnRowCommand="gvCustomers_RowCommand" 
+                    EmptyDataText="No Customers to see here">
+                    <Columns>
+                        <asp:BoundField DataField="Name" HeaderText="Name" />
+                        <asp:ButtonField CommandName="EditCustomer" Text="Edit" ControlStyle-CssClass="btn-grid a-l" />
+                        <asp:ButtonField CommandName="SelectCustomer" Text="Select" ControlStyle-CssClass="btn-grid a-l" />
+                    </Columns>
+                </asp:GridView>
+            </div>
+            <div class="p-3">
+
+                <!-- CUSTOMERS FORM -->
+                <div id="divCust" runat="server" class="card s-2" visible="false">
+                    <div class="card-header"><asp:Label ID="lblCustomer" runat="server" Text="Add Customer" /></div>
+                    <div class="card-body a-l">
+                        Name
+                        <asp:TextBox ID="txtCustName" runat="server" CssClass="form-control form-control-sm s-2 mb-2" />
+                        Latituse
+                        <asp:TextBox ID="txtCustLat" runat="server" CssClass="form-control form-control-sm s-2 mb-2" />
+                        Longitude
+                        <asp:TextBox ID="txtCustLong" runat="server" CssClass="form-control form-control-sm s-2" />
+                    </div>
+                    <div class="card-footer a-r">
+                        <asp:Button ID="btnCustSave" runat="server" CssClass="btn btn-sm" Text="Save" OnClick="btnCustSave_Click" />
+                        <asp:Button ID="btnCustBack" runat="server" CssClass="btn btn-sm" Text="Back" OnClick="btnCustBack_Click" />
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        <br />
+
+        <!-- CONTACTS -->
+        <div id="divContactsArea" runat="server" class="d-inline-flex flex-row flex-wrap" visible="false">
+            <div class="p-3">
+
+                <!-- CONTACTS GRID -->
+                <b>Contacts</b>
+                <asp:Button ID="btnAddCon" runat="server" CssClass="btn btn-sm btn-dark ml-2" Text="Add Contact" OnClick="btnAddCon_Click" />
+                <asp:GridView ID="gvContacts" runat="server" CssClass="tbl s-2 my-3" AutoGenerateColumns="False" DataKeyNames="ID" 
+                    EmptyDataText="No Contacts to see here" OnRowCommand="gvContacts_RowCommand">
+                    <Columns>
+                        <asp:BoundField DataField="Name" HeaderText="Name" />
+                        <asp:BoundField DataField="Email" HeaderText="Email" />
+                        <asp:BoundField DataField="ContactNumber" HeaderText="Number" />
+                        <asp:ButtonField CommandName="EditCustomer" Text="Edit" ControlStyle-CssClass="btn-grid a-l" />
+                    </Columns>
+                </asp:GridView>
+            </div>
+            <div class="p-3">
+
+                <!-- CONTACTS FORM -->
+                <div id="divCon" runat="server" class="card s-2" visible="false">
+                    <div class="card-header"><asp:Label ID="lblContact" runat="server" Text="Add Contact" /></div>
+                    <div class="card-body a-l">
+                        Name
+                        <asp:TextBox ID="txtConName" runat="server" CssClass="form-control form-control-sm s-2 mb-2" />
+                        Email
+                        <asp:TextBox ID="txtConEmail" runat="server" CssClass="form-control form-control-sm s-2 mb-2" TextMode="Email" />
+                        Contact Number
+                        <asp:TextBox ID="txtConNo" runat="server" CssClass="form-control form-control-sm s-2" TextMode="Number" />
+                    </div>
+                    <div class="card-footer a-r">
+                        <asp:Button ID="btnConSave" runat="server" CssClass="btn btn-sm" Text="Save" OnClick="btnConSave_Click" />
+                        <asp:Button ID="btnConBack" runat="server" CssClass="btn btn-sm" Text="Back" OnClick="btnConBack_Click" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    DataClassesDataContext _Datalayer = new DataClassesDataContext();
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        try
+        {
+            if (!IsPostBack)
+                btnCustBack_Click(sender, e);
+        }
+        catch( Exception ex){lblError.Text = "Error: " + ex.Message;}
+    }
+
+    #region CUSTOMER
+
+    protected void btnCustAdd_Click(object sender, EventArgs e) //Add Customer Button
+    {
+        try
+        {
+            btnCustBack_Click(sender, e);
+            divCust.Visible = true;
+            lblCustomer.Text = "Add Customer";
+        }
+        catch (Exception ex) { lblError.Text = "Error: " + ex.Message; }
+    }
+
+    protected void gvCustomers_RowCommand(object sender, GridViewCommandEventArgs e) //Edit / Select Customer
+    {
+        try
+        {
+            Customer _cust = _Datalayer.Customers.First(c => c.ID.ToString() == gvCustomers.DataKeys[Convert.ToInt32(e.CommandArgument)]["ID"].ToString());
+            hidCust.Value = _cust.ID.ToString();
+
+            if (e.CommandName == "EditCustomer")
+            {
+                divCust.Visible = true;
+                txtCustName.Text = _cust.Name;
+                txtCustLat.Text = _cust.Latitude;
+                txtCustLong.Text = _cust.Longitude;
+                lblCustomer.Text = "Edit Customer";
+                divContactsArea.Visible = false;
+            }
+            if (e.CommandName == "SelectCustomer")
+            {
+                gvContacts.Visible = true;
+                btnConBack_Click(sender, e);
+                divContactsArea.Visible = true;
+            }
+        }
+        catch (Exception ex) { lblError.Text = "Error: " + ex.Message; }
+    }
+
+    protected void btnCustSave_Click(object sender, EventArgs e) //Save Customer Button
+    {
+        try
+        {
+            Customer _cust;
+            if (string.IsNullOrEmpty(hidCust.Value)) //Add new Customer
+            {
+                _cust = new Customer();
+                _Datalayer.Customers.InsertOnSubmit(_cust);
+            }
+            else _cust = _Datalayer.Customers.First(c => c.ID.ToString() == hidCust.Value); //Update Customer
+
+            _cust.Name = txtCustName.Text;
+            _cust.Latitude = txtCustLat.Text;
+            _cust.Longitude = txtCustLong.Text;
+
+            _Datalayer.SubmitChanges();
+            btnCustBack_Click(sender, e);
+        }
+        catch (Exception ex) { lblError.Text = "Error: " + ex.Message; }
+    }
+
+    protected void btnCustBack_Click(object sender, EventArgs e)// Back Button
+    {
+        try
+        {
+            hidCust.Value = null;
+            txtCustName.Text = "";
+            txtCustLat.Text = "";
+            txtCustLong.Text = "";
+            divCust.Visible = false;
+            divContactsArea.Visible = false;
+            lblError.Text = "";
+
+            gvCustomers.DataSource = _Datalayer.Customers.OrderBy(c => c.Name).ToList();
+            gvCustomers.DataBind();
+        }
+        catch (Exception ex) { lblError.Text = "Error: " + ex.Message; }
+    }
+
+    #endregion
+
+    #region CONTACTS
+
+    protected void btnAddCon_Click(object sender, EventArgs e) //Add Contact Button
+    {
+        try
+        {
+            btnConBack_Click(sender, e);
+            divCon.Visible = true;
+            lblContact.Text = "Add Contact";
+        }
+        catch (Exception ex) { lblError.Text = "Error: " + ex.Message; }
+    }
+
+    protected void gvContacts_RowCommand(object sender, GridViewCommandEventArgs e) //Edit Contact
+    {
+        try
+        {
+            CustomerContact _con = _Datalayer.CustomerContacts.First(c => c.ID.ToString() == gvContacts.DataKeys[Convert.ToInt32(e.CommandArgument)]["ID"].ToString());
+            hidCon.Value = _con.ID.ToString();
+
+            if (e.CommandName == "EditCustomer")
+            {
+                divCon.Visible = true;
+                txtConEmail.Text = _con.Email;
+                txtConName.Text = _con.Name;
+                txtConNo.Text = _con.ContactNumber;
+                lblContact.Text = "Edit Contact";
+            }
+        }
+        catch (Exception ex) { lblError.Text = "Error: " + ex.Message; }
+    }
+
+    protected void btnConSave_Click(object sender, EventArgs e) //Save Contact Button
+    {
+        try
+        {
+            CustomerContact _con;
+            if (string.IsNullOrEmpty(hidCon.Value)) //Add new Contact
+            {
+                _con = new CustomerContact();
+                _Datalayer.CustomerContacts.InsertOnSubmit(_con);
+            }
+            else _con = _Datalayer.CustomerContacts.First(c => c.ID.ToString() == hidCon.Value); //Update Contacts
+
+            _con.CustomerID = Convert.ToInt32(hidCust.Value);
+            _con.Name = txtConName.Text;
+            _con.ContactNumber = txtConNo.Text;
+            _con.Email = txtConEmail.Text;
+
+            _Datalayer.SubmitChanges();
+            btnConBack_Click(sender, e);
+        }
+        catch (Exception ex) { lblError.Text = "Error: " + ex.Message; }
+    }
+
+    protected void btnConBack_Click(object sender, EventArgs e) //Back Button
+    {
+        try
+        {
+            hidCon.Value = null;
+            txtConEmail.Text = "";
+            txtConName.Text = "";
+            txtConNo.Text = "";
+            divCon.Visible = false;
+            lblError.Text = "";
+
+            //There is no need for a stored procedure in this scenario, but here is one as requested
+            gvContacts.DataSource = _Datalayer._sp_Contacts(Convert.ToInt32(hidCust.Value)).ToList();
+            gvContacts.DataBind();
+        }
+        catch (Exception ex) { lblError.Text = "Error: " + ex.Message; }
+    }
+
+    #endregion 
+}
